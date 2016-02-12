ab83cddda37c5d8bf32e0a48abdb40f7a6ba3b56
Randall Mardus <randalljmardus@gmail.com>
2016-02-12 17:19:25 -0500

    create user entity

---
+++ User+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  User+CoreDataProperties.swift
+//  Core Data UITableView
+//
+//  Created by Randall Mardus on 2/12/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension User {
+
+    @NSManaged var name: String?
+
+}
---
+++ User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Core Data UITableView
+//
+//  Created by Randall Mardus on 2/12/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class User: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
